d16a7047b1c59783cba4163ab2b4db10875c34da
Alex-vell <https://github.com/Alex-vell/Git-My-first-project.git>
2021-04-15 21:40:39 +0300

    lesson 55. Pagination

--- projects/01-first-project/my-app/src/components/Users/Users.jsx
+++ projects/01-first-project/my-app/src/components/Users/Users.jsx
@@ -6,15 +6,37 @@ import userPhoto from '../../assets/images/user.png'
 class Users extends React.Component {
     componentDidMount() {
         if (this.props.users.length === 0)
-            axios.get('https://social-network.samuraijs.com/api/1.0/users')
+            axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSise}`)
                 .then(response => {
                     this.props.setUsers(response.data.items);
+                    this.props.setTotalUserCount(response.data.totalCount);
                 });
     }
 
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage([pageNumber]);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSise}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+            });
+    }
+
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSise);
+
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i)
+        }
+
         return <div>
-            <button onClick={this.getUsers}>Get Users</button>
+            <div>
+                {pages.map(p => {
+                    return <span className={this.props.currentPage === p && styles.selectedPage}
+                                 onClick={ (e) => { this.onPageChanged(p) }}>{p}</span>
+                })}
+            </div>
             {
                 this.props.users.map(u => <div key={u.id}>
                 <span>
--- projects/01-first-project/my-app/src/components/Users/UsersContainer.jsx
+++ projects/01-first-project/my-app/src/components/Users/UsersContainer.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Users from "./Users";
 import {connect} from "react-redux";
-import {followAC, setUserAC, unfollowAC} from "../../redux/users-reducer";
+import {followAC, setCurrentPageAC, setUserAC, setUsersTotalCountAC, unfollowAC} from "../../redux/users-reducer";
 
 let mapStateToProps = (state) => {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        pageSise: state.usersPage.pageSise,
+        totalUsersCount: state.usersPage.totalUsersCount,
+        currentPage: state.usersPage.currentPage
     }
 }
 let mapDispatchToProps = (dispatch) => {
@@ -18,6 +21,12 @@ let mapDispatchToProps = (dispatch) => {
         },
         setUsers: (users) => {
             dispatch(setUserAC(users));
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUserCount: (totalCount) => {
+            dispatch(setUsersTotalCountAC(totalCount))
         }
     }
 }
