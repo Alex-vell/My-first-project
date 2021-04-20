319b80439a439749a4979360e8c3eacfbfbd8e3f
Alex-vell <https://github.com/Alex-vell/Git-My-first-project.git>
2021-04-20 23:39:04 +0300

    lesson 62. follow-unfollow api.

--- projects/01-first-project/my-app/src/components/Header/Header.jsx
+++ projects/01-first-project/my-app/src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import s from './Header.module.css';
+import NavLink from "react-router-dom/NavLink";
 
-const Header = () => {
+const Header = (props) => {
     return <header className={s.header}>
-    <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTskF1ENMQmiosFwUfUir1_F_A9iymAa2w62A&usqp=CAU' />
-  </header>
+        <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTskF1ENMQmiosFwUfUir1_F_A9iymAa2w62A&usqp=CAU'/>
+        <div className={s.loginBlock}>
+            {props.isAuth ? props.login
+                : <NavLink to={'/login'}>Login</NavLink>}
+        </div>
+    </header>
 }
 
 export default Header;
