ff33e3a730b35b456533bb84ee3c425b18a8bf01
Alex-vell <https://github.com/Alex-vell/Git-My-first-project.git>
2021-05-03 20:35:34 +0300

    lesson 74. componentDidUpdate.

--- projects/01-first-project/my-app/src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ projects/01-first-project/my-app/src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -25,18 +25,28 @@ class ProfileStatus extends React.Component {
         })
     }
 
+    componentDidUpdate(prevProps, prevState) {
+
+        if (prevProps.status !== this.props.status) {
+            this.setState({
+                status: this.props.status
+            });
+        }
+    }
+
     render() {
         return (
             <div>
                 {!this.state.editMode &&
-                    <div>
-                        <span onDoubleClick={ this.activateEditMode }>{this.props.status || '---'}</span>
-                    </div>
+                <div>
+                    <span onDoubleClick={this.activateEditMode}>{this.props.status || '---'}</span>
+                </div>
                 }
                 {this.state.editMode &&
-                    <div>
-                        <input onChange={this.onStatusChange} autoFocus={true} onBlur={ this.deactivateEditMode } value={this.state.status}/>
-                    </div>
+                <div>
+                    <input onChange={this.onStatusChange} autoFocus={true} onBlur={this.deactivateEditMode}
+                           value={this.state.status}/>
+                </div>
                 }
             </div>
         )
