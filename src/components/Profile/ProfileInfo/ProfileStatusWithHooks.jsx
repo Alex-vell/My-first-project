e504c441f4fe9b973b6a2f71928250d821b9c051
Alex-vell <https://github.com/Alex-vell/Git-My-first-project.git>
2021-05-13 23:36:38 +0300

    lesson 85. hook, useEffect.

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -1,10 +1,14 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import s from './ProfileInfo.module.css';
 
 const ProfileStatusWithHooks = (props) => {
 
     let [editMode, setEditMod] = useState(false);
     let [status, setStatus] = useState(props.status);
+
+    useEffect( () => {
+        setStatus(props.status);
+    }, [props.status]);
 
     const activateEditMode = () => {
         setEditMod(true);
